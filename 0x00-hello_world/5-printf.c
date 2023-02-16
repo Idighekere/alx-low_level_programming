c3fd224f179d757877bf2462cc0b58719c9ec074
Idighekere Udo <idighekereudo@gmail.com>
2023-02-16 14:15:26 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - a program that prints a new line with the printf command
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
