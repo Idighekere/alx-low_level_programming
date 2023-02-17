cf7bd614a396f7e765d3479699e590c6a2398308
Idighekere Udo <idighekereudo@gmail.com>
2023-02-17 15:04:18 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++; ch != 'q' && 'e')
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
