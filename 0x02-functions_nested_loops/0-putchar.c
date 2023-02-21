dfa3802452605319d70218f80835a8de146c0562
Idighekere Udo <idighekereudo@gmail.com>
2023-02-21 09:46:39 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 /**
  * main - a program that prints _putchar
  * Followed by a new line
@@ -6,8 +6,14 @@
  */
 int main(void)
 {
-	putchar("_putchar");
-
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
