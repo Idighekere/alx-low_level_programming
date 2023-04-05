16e0598e8e686d7361ea4c21865d434bbceadf38
idighekere udo <idighekereudo@gmail.com>
2023-04-04 17:55:34 -0700

    prints all element in a linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t display = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		display++;
 	}
 
-	return (count);
+	return (display);
 }
