8969e4edf36ab1347db026d7ec68a21aea79a36a
idighekere udo <idighekereudo@gmail.com>
2023-04-07 19:04:42 -0700

    coverts a binary no. to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,8 +3,8 @@
 
 /**
  * binary_to_uint - a function that converts a binary number to an unsigned int
- * @b: a pointer to the string of chars 0 and 1 
- * Return: the converted number or 0 if strings in b is not 0 or 1 
+ * @b: a pointer to the string of chars 0 and 1
+ * Return: the converted number or 0 if strings in b is not 0 or 1
  * b is NULL
  */
 
@@ -14,19 +14,18 @@ unsigned int binary_to_uint(const char *b)
 	int a = 0;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	do {
 		if (b[a] != '0' && b[a] != '1')
-			return 0;
-			
+			return (0);
+
 		numb <<= 1;
 		if (b[a] == '1')
 			numb |= 1;
 
 		a++;
-	}while (b[a] != '\0');
+	} while (b[a] != '\0');
 
-	return numb;
+	return (numb);
 }
-		
